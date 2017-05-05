83e740abdfd4baf6c7124b6eba2b4fb80739b534
sgbj <scott.batary@gmail.com>
2017-05-04 22:19:29 -0500

    more content

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,6 @@
-document.querySelector('video').playbackRate = 0.6;
+var video = document.querySelector('video');
+video.playbackRate = 0.6;
+video.play();
 
 window.sr = ScrollReveal();
 sr.reveal('.reveal');
